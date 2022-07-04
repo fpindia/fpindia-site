628b8b88e284204ed4ddc2550df09d5670bd7adb
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2022-07-04 10:58:35 -0400

    Add support for Markdown (#21)

--- src/FPIndia/Site.hs
+++ src/FPIndia/Site.hs
@@ -3,8 +3,10 @@
 
 module FPIndia.Site where
 
+import Data.Default (def)
 import Data.Generics.Sum.Any (AsAny (_As))
 import Ema
+import Ema.Route.Lib.Extra.MarkdownRoute qualified as MR
 import FPIndia.Model (Model (Model, modelStatic))
 import FPIndia.Route (Route (..), StaticRoute)
 import FPIndia.View (renderHtmlRoute)
@@ -13,7 +15,8 @@ import Optics.Core ((%))
 instance EmaSite Route where
   siteInput cliAct () = do
     staticRouteDyn <- siteInput @StaticRoute cliAct ()
-    pure $ Model <$> staticRouteDyn
+    markdownDyn <- siteInput @MR.MarkdownRoute cliAct $ def {MR.argBaseDir = "markdown"}
+    pure $ Model <$> staticRouteDyn <*> markdownDyn
   siteOutput rp m = \case
     Route_Html r ->
       Ema.AssetGenerated Ema.Html $ renderHtmlRoute rp m r
