b181399347b73a571523ca4a953484ca08ff971a
Sridhar Ratnakumar <srid@srid.ca>
2022-08-09 13:18:09 -0400

    Add attributes extension, which we use in events page

--- src/FPIndia/Site.hs
+++ src/FPIndia/Site.hs
@@ -12,11 +12,12 @@ import FPIndia.Model (Model (Model, modelStatic), PandocExts)
 import FPIndia.Route (Route (..), StaticRoute)
 import FPIndia.View (renderHtmlRoute)
 import Optics.Core ((%))
+import Text.Pandoc qualified as Pandoc
 
 instance EmaSite Route where
   siteInput cliAct () = do
     staticRouteDyn <- siteInput @StaticRoute cliAct ()
-    markdownDyn <- siteInput @(PR.PandocRoute PandocExts) cliAct $ def {PR.argBaseDir = "markdown"}
+    markdownDyn <- siteInput @(PR.PandocRoute PandocExts) cliAct markdownRouteArg
     jobsDyn <- Jobs.jobsDynamic "jobs/jobs.csv"
     pure $ Model <$> staticRouteDyn <*> markdownDyn <*> jobsDyn
   siteOutput rp m = \case
@@ -24,6 +25,17 @@ instance EmaSite Route where
       pure $ Ema.AssetGenerated Ema.Html $ renderHtmlRoute rp m r
     Route_Static r ->
       siteOutput (rp % (_As @"Route_Static")) (modelStatic m) r
+
+-- | Configuration for parsing and rendering ./markdown using Pandoc
+markdownRouteArg :: PR.Arg
+markdownRouteArg =
+  def
+    { PR.argBaseDir = "markdown"
+    , PR.argReaderOpts = def {Pandoc.readerExtensions = exts}
+    , PR.argWriterOpts = def {Pandoc.writerExtensions = exts}
+    }
+  where
+    exts = Pandoc.pandocExtensions <> Pandoc.extensionsFromList [Pandoc.Ext_attributes]
 
 runFPIndiaSite :: IO ()
 runFPIndiaSite = Ema.runSite_ @Route ()
