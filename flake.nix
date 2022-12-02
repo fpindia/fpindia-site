a37ff157170cb4d6dfb776fc31c43986f2738983
Sridhar Ratnakumar <srid@srid.ca>
2022-12-01 20:36:03 -0500

    Update nixpkgs, flake-parts; simplify Nix

--- flake.nix
+++ flake.nix
@@ -3,22 +3,15 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     haskell-flake.url = "github:srid/haskell-flake";
-
-    # Haskell overrides
-    ema.url = "github:srid/ema/0.8.0.0";
-    ema.flake = false;
-    tailwind-haskell.url = "github:srid/tailwind-haskell/master";
-    tailwind-haskell.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, haskell-flake, ... }:
+  outputs = { self, nixpkgs, flake-parts, haskell-flake, ... }:
     flake-parts.lib.mkFlake { inherit self; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
         haskell-flake.flakeModule
       ];
-      perSystem = { self', inputs', pkgs, ... }: {
+      perSystem = { self', inputs', pkgs, lib, ... }: {
         # "haskellProjects" comes from https://github.com/srid/haskell-flake
         haskellProjects.default = {
           root = ./.;
@@ -31,20 +24,12 @@
             inherit (hp)
               cabal-fmt
               fourmolu;
-            inherit (inputs'.tailwind-haskell.packages)
+            inherit (pkgs.haskellPackages)
               tailwind;
           };
-          source-overrides = {
-            inherit (inputs)
-              ema;
-          };
-          overrides = self: super: with pkgs.haskell.lib; {
-            ema = dontCheck super.ema;
-            inherit (inputs'.tailwind-haskell.packages)
-              tailwind;
-          };
+          overrides = self: super: { };
         };
-        apps.tailwind-run.program = "${inputs'.tailwind-haskell.packages.tailwind}/bin/tailwind-run";
+        apps.tailwind-run.program = "${lib.getExe pkgs.haskellPackages.tailwind}";
       };
     };
 }
